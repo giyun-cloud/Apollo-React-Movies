52968feef6d6ffed6120c42d31c242525ba7e035
giyun-cloud <nangiyoon@gmail.com>
2021-07-08 13:33:19 +0900

    1.0 Apollo Client ~ 1.1 GET MOVIES Query

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { gql, useQuery } from "@apollo/client";
+
+const GetMovies = gql`
+  {
+    movies {
+      id
+      title
+      medium_cover_image
+    }
+  }
+`;
 
 function Home() {
-  return <div>Home</div>;
+  const { loading, error, data } = useQuery(GetMovies);
+  if (loading) return <div>loading...</div>;
+  if (data && data.movies) return data.movies.map((m) => <div>{m.title}</div>);
 }
 
 export default Home;
