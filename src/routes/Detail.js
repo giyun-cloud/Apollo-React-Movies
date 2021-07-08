8324c43291d4279a18b9a100d37ff39ce4d8abef
giyun-cloud <nangiyoon@gmail.com>
2021-07-09 07:06:00 +0900

    1.2 GET MOVIE Query

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,11 +1,26 @@
-import React from 'react'
+import React from "react";
+import { useParams } from "react-router-dom";
+import { gql, useQuery } from "@apollo/client";
 
-function Detail () {
-  return (
-    <div>
-      Detail
-    </div>
-  )
+const GetMovie = gql`
+  query getMovie($id: Int!) {
+    movie(id: $id) {
+      id
+      title
+      medium_cover_image
+      description_intro
+    }
+  }
+`;
+
+function Detail() {
+  const { id } = useParams();
+  const { loading, data } = useQuery(GetMovie, {
+    variables: { id },
+  });
+  if (loading) return "loading";
+  if (data && data.movie) return data.movie.title;
+  return <div>oo</div>;
 }
 
-export default Detail
+export default Detail;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,20 +1,68 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const GetMovies = gql`
   {
     movies {
       id
-      title
       medium_cover_image
     }
   }
 `;
 
-function Home() {
-  const { loading, error, data } = useQuery(GetMovies);
-  if (loading) return <div>loading...</div>;
-  if (data && data.movies) return data.movies.map((m) => <div>{m.title}</div>);
-}
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
+
+const DataLink = styled(Link)``;
+
+const Home = () => {
+  const { loading, data } = useQuery(GetMovies);
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo 2022</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+      {data &&
+        data.movies &&
+        data.movies.map((m) => <DataLink to={`/${m.id}`}>{m.id}</DataLink>)}
+    </Container>
+  );
+};
 
 export default Home;
